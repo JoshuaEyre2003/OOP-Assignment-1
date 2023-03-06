95cdbcec8f0299b045597569c3ae8080dfd32417
Joshua Eyre <joshreyre2003@gmail.com>
2023-03-06 20:08:31 +0000

    Testing

--- CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -12,10 +12,12 @@ namespace CMP1903M_A01_2223
         {
             Pack pack = new Pack();
             string input;
+            
             Testing validation = new Testing();
             Console.WriteLine("Please enter an amount of cards(1,52)");
             input = Console.ReadLine();
-            validation.Validation(input,52,1,0);
+            int intInput = validation.Validation(input,52,1,0);
+            
         }
     }
 }
