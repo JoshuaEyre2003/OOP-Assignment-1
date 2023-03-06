7419099cc6f5f23540e0c1db5436af412463b89d
Joshua Eyre <joshreyre2003@gmail.com>
2023-03-06 19:12:17 +0000

    Validation Complete

---
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Testing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Testing
+    {
+        public Testing()
+        {
+
+        }
+        public string Validation(string input,int maxLength,int minLength)
+        {
+            int refinedinput;
+            bool validated = false;
+            while (validated == false)
+            {
+                try
+                {
+                    refinedinput = Convert.ToInt32(input);
+                    if (refinedinput >= minLength && refinedinput <= maxLength)
+                    {
+                        validated = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Please enter a number within {maxLength} and {minLength},Try again");
+                        input = Console.ReadLine();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a Correct type.Try again");
+                    input = Console.ReadLine();
+                }
+
+            }
+
+            return input ;
+        }
+    }
+}
